ec1069535cbb96315376e655369d2cd125105ab3
JulioLopez <juliojlgon@gmail.com>
2015-03-11 11:22:34 +0100

    Clase reserva creada

---
+++ src/java/Helper/Reserva.java
@@ -0,0 +1,16 @@
+/**
+ * Sistemas distribuidos - Practica Hotel
+ * Universidad Rey Juan Carlos, Mostoles
+ * @author Julio Lopez González
+ * @author Manuel Gómez Pérez
+ * @see https://github.com/giiade/hotel-distribuido
+ */
+package Helper;
+
+/**
+ *
+ * 
+ */
+public class Reserva {
+    
+}
