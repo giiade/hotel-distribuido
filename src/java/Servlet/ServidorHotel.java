cf0a59ebabe41c390f399123efbed9ba185f2dac
JulioLopez <juliojlgon@gmail.com>
2015-03-15 20:12:43 +0100

    Servlet contructor y clase de constantes creada. FALLOS

--- src/java/Servlet/ServidorHotel.java
+++ src/java/Servlet/ServidorHotel.java
@@ -7,8 +7,13 @@
  */
 package Servlet;
 
+import Helper.Huesped;
+import Helper.Reserva;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,8 +24,37 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author JulioLopez
  */
-@WebServlet(name = "ServidorHotel", urlPatterns = {"/ServidorHotel"})
+@WebServlet(name = "ServidorHotel", urlPatterns = {"/consultarHuesped"}, loadOnStartup = -1)
 public class ServidorHotel extends HttpServlet {
+    
+    ArrayList<Huesped> huespedes = new ArrayList<>();
+    HashMap<String,ArrayList<Reserva>> reservasCliente = new HashMap<>();
+    ArrayList<Reserva> reservas = new ArrayList<>();
+
+    
+    
+    
+    
+    
+
+    /**
+     * Initilizes the servlet with config file and saved data if it exist
+     * @param config -> Configuration of server
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        //Añadiriamos aquí un archivo file para cargar al inciar el servlet.
+        //Crearemos los ejemplos en el caso de que no encuentre un archivo.    
+        
+        super.init(config); 
+        
+        
+        
+    }
+    
+    
+    
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -33,18 +67,49 @@ public class ServidorHotel extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        Huesped huesped = new Huesped(); // Constructor vacio, datos ejemplo
+        Reserva reserva = new Reserva(); // Constructor vacio, datos ejemplo
+        System.out.println("Iniciando el server....");
+        
+        huespedes.add(huesped);
+        
+        reservas.add(reserva);      
+        reservasCliente.put(reserva.getNIF(), reservas);
+        
+        
+        String webPath = request.getServletPath();
+        ArrayList<String> resultados = new ArrayList<>();
+        
+        System.out.println("Peticion recibida -->" +  webPath);
+        
+        switch(webPath){
+            case "/consultarHuesped":
+                int nParametros = request.getParameterMap().size();
+                if(nParametros > 0){
+                    //String a consultar con el servidor
+                     String consulta = request.getParameter("NIF");
+                     
+                     
+                     //Consultamos con nuestro ArrayList de servidores.
+                     
+                     for(Huesped h:huespedes){
+                         if (h.getNif().equals(consulta))
+                             resultados.add(h.toString());
+                     }
+                }
+                
+        }
+        
+        if (resultados.isEmpty()){
+            resultados.add("No se ha encontrado nada");
+        }
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServidorHotel</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServidorHotel at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            for (String i: resultados){
+                out.println(i);
+            }
+            
         }
     }
 
